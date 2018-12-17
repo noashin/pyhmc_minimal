a83657087eb85ec7908cb1d388aa6746ca918bfd
Noa Shinitski <malem@uni-potsdam.de>
2018-12-17 16:08:04 +0100

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyhmc_minimal',
+      version='0.1',
+      description='A minimal implementation for the Hamiltonian Monte Carlo algorithm',
+      url='https://github.com/noashin/pyhmc_minimal',
+      author='Flying Circus',
+      author_email='noa.shinitski@gmail.com',
+      license='MIT',
+      packages=['pyhmc_minimal'],
+      zip_safe=False)
